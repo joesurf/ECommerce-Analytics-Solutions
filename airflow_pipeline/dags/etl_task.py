e3a03d2fc41f55c65f940bcbcdf7d2a1693fecfb
joesurf <desictiverk@gmail.com>
2024-03-27 03:17:16 +0800

    Update sql database connection

--- airflow_pipeline/dags/etl_task.py
+++ airflow_pipeline/dags/etl_task.py
@@ -17,8 +17,8 @@ with DAG(
     dag_id='tables_inner_join_etl',
     default_args=default_args,
     description='sample',
-    start_date=datetime(2024, 3, 28, 2),
-    # schedule_interval='@daily'
+    start_date=datetime(2024, 3, 20, 2),
+    schedule_interval='@daily'
 ) as dag:
 
     run_etl = PythonOperator(
@@ -27,12 +27,10 @@ with DAG(
         dag=dag
     )
 
-    run_etl
+    run_this = BashOperator(
+        task_id="run_starting",
+        bash_command="echo Starting ETL Process...",
+        dag=dag
+    )
 
-# run_this = BashOperator(
-#     task_id="run_after_loop",
-#     bash_command="echo 1",
-#     dag=dag
-# )
-
-# run_this.set_downstream(run_etl)
+    run_this.set_downstream(run_etl)
--- airflow_pipeline/dags/sql_helper.py
+++ airflow_pipeline/dags/sql_helper.py
@@ -1,15 +1,10 @@
-import logging
+import os
 
 from sqlalchemy import create_engine
-
 import pandas as pd
-
 from dotenv import load_dotenv
 
 load_dotenv()
-
-MYSQL_PASSWORD='Wolfer#2164' # BT4301admin
-
 
 
 def run_mysql_etl():
@@ -17,40 +12,27 @@ def run_mysql_etl():
 """
 SELECT *
 FROM customer AS c
-INNER JOIN orders AS o ON c.customer_id = o.customer_id
-INNER JOIN order_reviews AS r ON o.order_id = r.order_id
-INNER JOIN order_items AS i ON o.order_id = i.order_id
-INNER JOIN products AS p ON i.product_id = p.product_id
-INNER JOIN order_payments AS pay ON o.order_id = pay.order_id
-INNER JOIN sellers AS s ON i.seller_id = s.seller_id
-INNER JOIN product_category_name AS ct ON p.product_category_name = ct.product_category_name;
+    INNER JOIN orders AS o ON c.customer_id = o.customer_id
+    INNER JOIN order_reviews AS r ON o.order_id = r.order_id
+    INNER JOIN order_items AS i ON o.order_id = i.order_id
+    INNER JOIN products AS p ON i.product_id = p.product_id
+    INNER JOIN order_payments AS pay ON o.order_id = pay.order_id
+    INNER JOIN sellers AS s ON i.seller_id = s.seller_id
+    INNER JOIN product_category_name AS ct ON p.product_category_name = ct.product_category_name;
 """
 
-    # engine = create_engine(f"mysql://root:{MYSQL_PASSWORD}@127.0.0.1:3306/ecommerce", echo=False)
-    # db_ecommerce = engine.connect()
+    engine = create_engine(f"mysql://admin:{os.environ['MYSQL_PASSWORD']}@{os.environ['HOST']}:3306/ecommerce", echo=False)
+    db_ecommerce = engine.connect()
 
-    engine = create_engine(f"mysql://root:{MYSQL_PASSWORD}@127.0.0.1:3306/ecommerce_datawarehouse", echo=False)
+    engine = create_engine(f"mysql://admin:{os.environ['MYSQL_PASSWORD']}@{os.environ['HOST']}:3306/ecommerce_datawarehouse", echo=False)
     db_ecommerce_warehouse = engine.connect()
 
-    # df = pd.read_sql(sql=merge_query, con=db_ecommerce)
+    df = pd.read_sql(sql=merge_query, con=db_ecommerce)
+    df.to_sql(name='main', con=db_ecommerce_warehouse, if_exists='replace')
 
-    data = {
-        'Name': ['John', 'Anna', 'Peter', 'Linda'],
-        'Age': [28, 35, 45, 30],
-        'Gender': ['Male', 'Female', 'Male', 'Female'],
-        'City': ['New York', 'Paris', 'London', 'Tokyo']
-    }
-
-    df = pd.DataFrame(data)
-
-    df.to_sql(name='person', con=db_ecommerce_warehouse, if_exists='replace')
-    db_ecommerce_warehouse.commit()
-
-    # db_ecommerce.close()
-    db_ecommerce_warehouse.close()
-
-    
+    db_ecommerce.close()
+    db_ecommerce_warehouse.close()    
 
 
-# if __name__ == '__main__':
-#     run_mysql_etl()
+if __name__ == '__main__':
+    run_mysql_etl()
